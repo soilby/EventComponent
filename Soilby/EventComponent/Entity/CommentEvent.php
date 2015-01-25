ba40f0b6f5bfe6ef0d2329ee419656f041552ea2
George Novik <grgnvk@gmail.com>
2015-01-25 20:15:59 +0300

    Push code

---
+++ Soilby/EventComponent/Entity/CommentEvent.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fliak
+ * Date: 23.1.15
+ * Time: 17.53
+ */
+
+namespace Soilby\EventComponent\Entity;
+
+
+/**
+ * Class CommentEvent
+ * @package Soilby\EventComponent\Entity
+ */
+class CommentEvent extends GenericEvent {
+
+
+    /**
+     * @var string
+     */
+    protected $comment;
+
+}
